bcc7056c39332d9db9f9a402fc5ce8ab8fb871af
Biju Kunjummen <biju.kunjummen@gmail.com>
2018-09-09 20:20:09 -0700

    Fixed UI

--- applications/sample-client-app/ui/src/main-view/client-calls.js
+++ applications/sample-client-app/ui/src/main-view/client-calls.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function makePassthroughCall(payload, delay, throw_exception = false) {
-  // const url = "/passthrough/messages"
-  const url = "http://localhost:8080/passthrough/messages"
+  const url = "/passthrough/messages"
+  // const url = "http://localhost:8080/passthrough/messages"
   return axios.post(url, {payload: payload, delay: delay, throw_exception});
 }
